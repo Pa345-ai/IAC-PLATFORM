479194d2fa9739d8dfbb50f763a1d0aad651590d
Pa345-ai <parameeranasinghe33@gmail.com>
2026-01-06 16:15:55 +0530

    Delete Bootstrap/output.tf

--- Bootstrap/output.tf
+++
@@ -1,9 +0,0 @@
-output "state_bucket_name" {
-  description = "S3 bucket for Terraform state"
-  value       = aws_s3_bucket.terraform_state.bucket
-}
-
-output "lock_table_name" {
-  description = "DynamoDB table for locks"
-  value       = aws_dynamodb_table.terraform_locks.name
-}
