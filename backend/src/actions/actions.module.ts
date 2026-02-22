bc78f78b4c2fe62ece716773b0c1ae98d6085529
Pa345-ai <parameeranasinghe33@gmail.com>
2026-02-22 13:48:02 +0530

    Delete backend directory

--- backend/src/actions/actions.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ActionsService } from './actions.service';
-
-@Module({
-  providers: [ActionsService],
-  exports: [ActionsService],
-})
-export class ActionsModule {}
